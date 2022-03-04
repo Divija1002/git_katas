625bbafbb8dd5906341a41f051070f1a72b1230c
Divija <divija.amaram@gmail.com>
2022-03-04 22:44:35 +0530

    task 7 first commit main

---
+++ merge_sort.c
@@ -0,0 +1,11 @@
+int main() {
+  int arr[] = {6, 5, 12, 10, 9, 1};
+  int size = sizeof(arr) / sizeof(arr[0]);
+
+  mergeSort(arr, 0, size - 1);
+
+  printf("Sorted array: \n");
+  for (int i = 0; i < size; i++)
+    printf("%d ", arr[i]);
+  printf("\n");
+}
